65e42585d80b572bbdf471e4d79ecf64cf7329f6
Carl <carlankorhillam@gmail.com>
2024-10-17 19:42:23 -0500

    added drive train code and added new controller wrapper to TeleOpTest.java

--- TeamCode/src/main/java/DriveTrain.java
+++ TeamCode/src/main/java/DriveTrain.java
@@ -1,14 +1,16 @@
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import java.lang.Math;
 
 public class DriveTrain{
+    public DcMotor LFDrive = null;
+    public DcMotor RFDrive = null;
+    public DcMotor LBDrive = null;
+    public DcMotor RBDrive = null;
+
     public DriveTrain(OpMode op) {
-        DcMotor LFDrive = null;
-        DcMotor RFDrive = null;
-        DcMotor LBDrive = null;
-        DcMotor RBDrive = null;
+
 
 
         LFDrive = op.hardwareMap.get(DcMotor.class, "fl");
@@ -26,4 +28,53 @@ public class DriveTrain{
         RFDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         RBDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+    public void RunToTargetAndWait(DcMotor Drive,int Counts, double Pow){
+        Drive.setTargetPosition(Drive.getCurrentPosition() + Counts);
+        Drive.setPower(Pow);
+        Drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while(Drive.isBusy()){}
+    }
+    public void RunToTarget(DcMotor Drive,int Counts, double Pow){
+        Drive.setTargetPosition(Drive.getCurrentPosition() + Counts);
+        Drive.setPower(Pow);
+        Drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    public int CalcMotorRatio(double C,Double PPR){
+        double CircPi = C * Math.PI;
+        return((int) ((1 / CircPi) * (PPR)));
+    }
+    public void MoveToPos(double Dist, double Pow){
+        double mm = Dist * (25.4 / 1);
+        int Counts = (int) (CalcMotorRatio(100,537.7) * mm);
+
+        RunToTarget(RFDrive,Counts,Pow);
+        RunToTarget(LFDrive,Counts,Pow);
+        RunToTarget(RBDrive,Counts,Pow);
+        RunToTarget(LBDrive,Counts,Pow);
+    }
+
+    public void DebugWheels(double Dist, double Pow){
+        double mm = Dist * (25.4 / 1);
+        int Counts = (int) (CalcMotorRatio(100,537.7) * mm);
+
+
+        RunToTargetAndWait(RFDrive,Counts,Pow);
+        RunToTargetAndWait(LFDrive,Counts,Pow);
+        RunToTargetAndWait(RBDrive,Counts,Pow);
+        RunToTargetAndWait(LBDrive,Counts,Pow);
+    }
+    public void KenematicMove(double y, double x, double turn, double Power){
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(turn), Power);
+        double frontLeftPower = (y + x + turn) / denominator;
+        double frontRightPower = (y - x - turn) / denominator;
+        double backRightPower = (y + x - turn) / denominator;
+        double backLeftPower = (y - x + turn) / denominator;
+
+        LFDrive.setPower(frontLeftPower);
+        LBDrive.setPower(backLeftPower);
+        RFDrive.setPower(frontRightPower);
+        RBDrive.setPower(backRightPower);
+    }
+    // Send calculated power to wheels
+
 }
